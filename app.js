0bb092f356eafb856a1f5ed3ac65b502dfeff034
Vishnu Ramnineni <vishnu.ramineni.05@gmail.com>
2015-08-04 17:02:25 +0530

    removed Hello

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var users = require('./routes/users');
 
 var app = express();
 //hello world
-//hello
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
